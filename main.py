373ff1393112df0d0b701856b5f7ab47f9cdaa56
KimTiger <124243611+ahekrltql@users.noreply.github.com>
2025-11-22 22:24:45 +0900

    Enhance PDF interaction with user input and streaming
    Added functionality for user input and streaming response handling.

--- main.py
+++ main.py
@@ -14,14 +14,24 @@ from langchain_classic.retrievers.multi_query import MultiQueryRetriever
 from langchain_classic import hub
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
+from streamlit_extras.buy_me_a_coffee import button
+from langchain_core.callbacks.base import BaseCallbackHandler
+#from dotenv import load_dotenv
+#load_dotenv()
 
 # 제목
 st.title("ChatPDF")
 st.write("---")
 
+#OpenAI 키 입력받기
+openai_key = st.text.input('OPEN_AI_API_KEY', type="password")
+
 # 파일 업로드
 uploaded_file = st.file_uploader("PDF 파일을 올려주세요.", type=["pdf"])
 st.write("---")
+
+#Buy me a coffee
+button(username="ahekrltql", floating=True, width=221)
 
 def pdf_to_document(uploaded_file):
     # 업로드된 파일을 임시 파일로 저장 후 Document 리스트로 로드
@@ -54,10 +64,20 @@ texts = text_splitter.split_documents(docs)
 # Embedding
 embeddings_model = OpenAIEmbeddings(
     model="text-embedding-3-large",
+    openai_api_key=openai_key
 )
 
 # Chroma DB
 db = Chroma.from_documents(texts, embeddings_model)
+
+# 스트리밍을 처리할 Handler 생성
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text=""):
+        self.container = container
+        self.text=initial_text
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text+=token
+        self.container.markdown(self.text)
 
 # User Input
 st.header("PDF에게 질문해보세요!")
@@ -75,13 +95,15 @@ if st.button("질문하기") and question:
         prompt = hub.pull("rlm/rag-prompt")
 
         # Generate
+        chat_box = st.empty()
+        stream_handler = StreamHandler(chat_box)
+        generate_llm = ChatOpenAI(model="gpt-4o-mini",temperature=0, openai_api_key=openai_key, streaming=True, callbacks=[stream_handler])
         def format_docs(docs):
             return "\n\n".join(doc.page_content for doc in docs)
-
         rag_chain = (
             {"context": retriever_from_llm | format_docs, "question": RunnablePassthrough()}
             | prompt
-            | llm
+            | generate_llm
             | StrOutputParser()
         )
 
